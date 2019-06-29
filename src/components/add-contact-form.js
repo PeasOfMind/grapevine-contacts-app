8e4a5f14d31ac51c327515645ac37f660742aef6
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-06-28 21:00:39 -0400

    Update contact form to use final-form

--- src/components/add-contact-form.js
+++ src/components/add-contact-form.js
@@ -1,40 +1,105 @@
 import React, { Component } from "react";
+import { Form, Field } from "react-final-form";
+import { Link } from "react-router-dom";
+import { API_BASE_URL } from "../config";
 
 export default class AddContactForm extends Component {
-  // constructor() {
-  //   this.state = {};
-  // }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log(event.target);
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      success: null
+    };
   }
 
+  onSubmit = values => {
+    this.setState({ success: null, error: null });
+    fetch(`${API_BASE_URL}/contacts`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify(values)
+    })
+      .then(res => res.json())
+      .then(resJson => {
+        this.setState({ success: "Your contact was added!", error: null });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ error: `Error! ${error.message}`, success: null });
+      });
+  };
+
   render() {
+    const required = value => (value ? undefined : "Required");
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input name="displayName" type="text" />
-        </label>
-        <br />
-        <label>
-          Title:
-          <input name="title" type="text" />
-        </label>
-        <br />
-        <label>
-          Company:
-          <input name="company" type="text" />
-        </label>
-        <br />
-        <label>
-          Location:
-          <input name="location" type="text" />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <>
+        <Form
+          onSubmit={this.onSubmit}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form>
+              <h2>Add a Contact</h2>
+              <Field name="displayName" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <label>Name:</label>
+                    <input {...input} type="text" placeholder="John Doe" />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="title">
+                {({ input, meta }) => (
+                  <div>
+                    <label>Title:</label>
+                    <input {...input} type="text" placeholder="ABC Executive" />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="company">
+                {({ input, meta }) => (
+                  <div>
+                    <label>Company:</label>
+                    <input {...input} type="text" placeholder="Example Inc." />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="location">
+                {({ input, meta }) => (
+                  <div>
+                    <label>Location:</label>
+                    <input {...input} type="text" placeholder="New York" />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <br />
+              <button
+                onClick={event => {
+                  event.preventDefault();
+                  handleSubmit();
+                }}
+                disabled={submitting}
+              >
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
+              <br />
+              {this.state.success}
+              {this.state.error}
+            </form>
+          )}
+        />
+        <Link to="/">Back to Contact Listings</Link>
+      </>
     );
   }
 }
