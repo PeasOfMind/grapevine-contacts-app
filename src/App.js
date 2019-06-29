8383596b1a4beef00473b682321ad850aed6ca67
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-06-28 20:59:48 -0400

    Setup route handling with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import AddContactForm from "./components/add-contact-form";
 import ContactListings from "./components/contact-listings";
+import AddContactForm from "./components/add-contact-form";
 
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <h1>Welcome</h1>
-        <AddContactForm />
-        <ContactListings />
+        <h1>Grapevine Contacts App</h1>
+        <Route exact path="/" component={ContactListings}/>
+        <Route path="/addContact" component={AddContactForm} />
       </div>
     );
   }
